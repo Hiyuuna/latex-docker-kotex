c9a355da1ac2c16ebf1c0a843ba9a1638ec5cacb
조유나 <126624365+Hiyuuna@users.noreply.github.com>
2024-04-29 06:52:25 +0000

    local change

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-IMAGE=blang/latex:ctanbasic
+IMAGE=sha256:0b66ce88f2e14638e766c8813f26bc34f627daadb3ce6693cee2572797abca6c
+#IMAGE=blang/latex:ctanbasic
 exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
